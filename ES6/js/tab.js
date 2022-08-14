bab982bf3980abefb7ccaea973d6a89b743db2bd
Benn314 <209441806@qq.com>
2022-08-15 00:41:49 +0800

    update

--- ES6/js/tab.js
+++ ES6/js/tab.js
@@ -5,8 +5,12 @@ class Tab {
     that = this;
     //获取元素
     this.main = document.querySelector(id);
-    this.lis = this.main.querySelectorAll("li");
-    this.sections = this.main.querySelectorAll("section");
+    this.add = this.main.querySelector(".tabadd");
+    // li的父元素
+    this.ul = this.main.querySelector(".firstnav ul:first-child");
+    //section 父元素
+    this.fsection = this.main.querySelector(".tabscon");
+
     this.init();
     /* 
         放构造函数里就不用自己手动调用了new一个对象的时候就会调用
@@ -14,14 +18,26 @@ class Tab {
   }
 
   init() {
+    this.updateNode();
     //init 初始化操作让相关的元素绑定事件
+    this.add.onclick = this.addTab;
     for (var i = 0; i < this.lis.length; i++) {
       this.lis[i].index = i;
       //   this.lis[i].onclick = function () {
       //     console.log(this.index);
       //   };
       this.lis[i].onclick = this.toggleTab;
+      this.remove[i].onclick = this.removeTab;
+      this.spans[i].ondbclick = this.editTab;
     }
+  }
+  //获取所有的小li 和 section
+  //因为我们动态添加元素 需要重新获取对应的元素
+  updateNode() {
+    this.lis = this.main.querySelectorAll("li");
+    this.sections = this.main.querySelectorAll("section");
+    this.remove = this.main.querySelectorAll(".icon-guanbi");
+    this.spans = this.main.querySelectorAll(".firstnav li span:first-child");
   }
   // 1 切换功能
   toggleTab() {
@@ -33,6 +49,7 @@ class Tab {
     //liactive是将tab标签变成选中的样式
     // conactive是显示选中标签的内容
   }
+  //清除所有li 和section 的类
   clearClass() {
     //让没被选中标签不要显示其内容
     for (var i = 0; i < this.lis.length; i++) {
@@ -41,11 +58,48 @@ class Tab {
     }
   }
   // 2 添加功能
-  addTab() {}
+  addTab() {
+    // alert(11);
+    that.clearClass();
+    // 1 创建li元素和section元素
+    var random = Math.random();
+    var li =
+      '<li class="liactive"><span>选项卡</span><span class="iconfont icon-guanbi"></span></li>';
+    var section = '<section class="conactive">测试' + random + "</section>";
+    // 2 把这两个元素追加到对应的父元素里面
+    that.ul.insertAdjacentHTML("beforeend", li); //注意这里是that
+    that.fsection.insertAdjacentHTML("beforeend", section);
+    that.init();
+  }
   // 3 删除功能
-  removeTab() {}
+  removeTab(e) {
+    e.stopPropagation(); //阻止冒泡 防止触发li 的切换点击事件
+    var index = this.parentNode.index; //让它直接对应父亲li的索引就好了
+    console.log(index);
+    //根据索引号删除对应的li 和section  remove()方法可以直接删除指定的元素
+    that.lis[index].remove();
+    that.section[index].remove();
+    that.init();
+    // 【优化】 当我们删除的不是选中状态的li 的时候 原来的选中状态li保持不变
+    if (document.querySelector(".liactive")) return;
+
+    //当我们删除了选中状态的这个li的时候 让它的前一个li处于选定状态
+    index--;
+    //手动调用我们的点击事件 不需要鼠标触发
+    that.lis[index] && that.lis[index].click();
+    //这一步是因为当页面只剩最后一个的时候
+    // 直接执行 that.lis[index].click(); 会报错 因为没有哪个点击事件的索引值为-1
+    //所以要做一个判断 判断索引值是否存在 存在则执行that.lis[index].click();让它的前一个li处于选定状态
+  }
   // 4 修改功能
-  eaditTab() {}
+  editTab() {
+    //双击禁止选定文字
+    // window.getSelection
+    //   ? window.getSelection().removeAllRanges()
+    //   : document.selection.empty();
+    alert(11);
+    // this.innerHTML = '<input type="text" />';
+  }
 }
 
 // var tab=new Tab("#tab");
