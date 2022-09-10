a9ca96a18b2aa0c14d2c129bdb444542c796fff0
Benn314 <209441806@qq.com>
2022-09-10 11:18:06 +0800

    update

--- Vue/Vue项目/去哪儿网/去哪儿网项目实战/travel/src/main.js
+++ Vue/Vue项目/去哪儿网/去哪儿网项目实战/travel/src/main.js
@@ -6,9 +6,9 @@ import router from './router' // router的index.js里导入了Home.vue 所以Vue
 import fastClick from 'fastclick'
 // 不知道为什么style里的这三个css文件运行会报错 找不到dependency依赖
 // 原因 视频源码的css文件的存放路径跟你不同 所谓的dependency依赖 就是没找到这个文件
-import './assets/styles/reset.css' // 适配移动端不同机型 正常显示1倍屏的数据在不同倍屏的情况下
-import './assets/styles/border.css'
-import './assets/styles/iconfont.css'
+import 'styles/reset.css' // 适配移动端不同机型 正常显示1倍屏的数据在不同倍屏的情况下
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body) // 这样 fastClick就设置完成咯 移动端点击的300ms问题就被解决了
--- Vue/Vue项目/去哪儿网/去哪儿网项目实战/travel/src/pages/home/components/Header.vue
+++ Vue/Vue项目/去哪儿网/去哪儿网项目实战/travel/src/pages/home/components/Header.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <div class="header">
-      <div class="header-left">返回</div>
-      <div class="header-input">输入城市/景点/游玩主题</div>
-      <div class="header-right">城市</div>
+      <div class="header-left">
+        <span class="iconfont back-icon">&#xe624;</span>
+      </div>
+      <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
+        </div>
+      <div class="header-right">
+        城市
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
     </div>
   </div>
 </template>
@@ -15,26 +23,36 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  /* @import '../../../assets/styles/varibles.styl' */
+  /* @import '~@/assets/styles/varibles.styl' */
+  @import '~styles/varibles.styl'
   .header
     display:flex
     line-height: .86rem
-    background: #00bcd4
+    background: $bgColor
     color: #fff
     width: 400px
+    border-radius: .1rem
+    margin:auto
     .header-left
       width: .64rem
       float: left
+      width 1.24rem
     .header-input
       flex:1
       line-height: .64rem
       height: .64rem
       margin-top: .12rem
-      margin-left: .2rem
       background: #fff
       border-radius: .1rem
       color: #ccc
+      text-align: left
+      padding-left: .2rem
     .header-right
       width 1.24rem
       float:right
       text-align: center
+      .arrow-icon
+        margin-left: -.02rem
+        font-size: .24rem
 </style>
